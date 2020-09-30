9588822894bff0a7b7e2d53a1ebde937a95948d8
Vikram Shinde <Shindevikram1210@gmail.com>
2020-10-01 05:13:23 +0530

    Added assembly language codes

---
+++ a64.asm
@@ -0,0 +1,166 @@
+%macro scall 4
+	mov rax,%1
+	mov rdi,%2
+	mov rsi,%3
+	mov rdx,%4
+	syscall
+%endmacro
+
+section .data
+
+	m1 db "Enter How many numbers u want to store in array",10
+	l1 equ $-m1
+	
+	m2 db "Enter the number",10
+	l2 equ $-m2
+	
+	m3 db " Array contents are=",10
+	l3 equ $-m3
+
+	m4 db "Addition of array elements are",10
+	l4 equ $-m4
+		
+	m5 db "",10
+	l5 equ $-m5
+	
+section .bss
+	num resb 20
+	cnt resb 20
+	cnt1 resb 20
+	cnt2 resb 20
+	array resb 200
+	char_ans resb 16
+
+section .text
+global _start
+
+_start:
+
+;--------------------accept array size-------------------------
+	scall 1,1,m1,l1
+	scall 0,0,num,3	
+	
+	call accept_proc
+	
+	mov [cnt],bx
+	mov [cnt1],bx
+	mov [cnt2],bx
+;--------------------accept array elements according array size----------------
+	
+	mov rbp,array
+up1:
+	scall 1,1,m2,l2
+	scall 0,0,num,3
+	
+	call accept_proc
+	
+	mov [rbp],bx
+	add rbp,2
+	dec byte[cnt]
+	jnz up1
+;-----------------------display array elements-----------------------------	
+	scall 1,1,m3,l3
+
+	mov rbx,array
+
+	mov ax,[cnt1]
+	mov [cnt],ax
+up2:
+	mov ax,[rbx]
+
+	call display_proc
+
+	add rbx,2
+
+	dec byte[cnt]
+
+	jnz up2	
+
+;-----------------------Addition of Array Elements-----------------------------
+	
+	scall 1,1,m4,l4
+	
+back1:
+	mov ax,[cnt2]
+	mov [cnt],ax
+	
+	mov ax,00h
+	mov bx,00h
+
+	mov rbx,array
+
+back2:
+
+	add ax,[rbx]
+	add rbx,2
+
+	dec byte[cnt]
+
+	jnz back2
+
+	call display_proc
+	
+
+
+	
+	
+;----------------------exit---------------------	
+	mov rax,60
+	mov rdi,0
+	syscall
+
+
+;-----------------------accept procedure--------------------	
+accept_proc:
+	mov rsi,num
+	mov rbx,0
+	mov rax,0
+	mov rcx,2
+back:
+	rol rbx,04
+	mov al,[rsi]
+	
+	cmp al,39h
+	jbe next
+	sub al,07h
+next:
+
+	sub al,30h
+	add bx,ax
+	
+	inc rsi
+	dec rcx
+	jnz back
+ret	
+
+;-----------------dispaly procedure-----------------------------
+
+display_proc:
+	
+	mov rbp,char_ans
+	mov rcx,2
+
+up3:
+	rol al,04
+	mov dl,al
+
+	and dl,0Fh
+
+	cmp dl,09h
+	jbe next1
+
+	add dl,07h
+next1:
+	add dl,30h
+
+	mov [rbp],dl
+
+	inc rbp
+	dec rcx
+
+	jnz up3
+
+	scall 1,1,char_ans,3
+	scall 1,1,m5,l5
+ret
+
---
+++ hex.asm
@@ -0,0 +1,221 @@
+%macro print 2   ;macro to print
+mov rax,1
+mov rdi,1
+mov rsi,%1
+mov rdx,%2
+syscall
+%endmacro
+
+%macro accept 2   ;macro to accept
+mov rax,0
+mov rdi,0
+mov rsi,%1
+mov rdx,%2
+syscall
+%endmacro
+
+SECTION .data
+msg1: db "Enter hex number: "
+len1: equ $-msg1
+msg2: db "BCD number: "
+len2: equ $-msg2
+msg3: db "Enter BCD number: "
+len3: equ $-msg3
+msg4: db "Hex number: "
+len4: equ $-msg4
+msg5: db "",10
+len5: equ $-msg5
+msg6: db "1. Hex to BCD ",10,"2. BCD to hex",10,"3. Exit",10,"Enter your choice",10
+len6: equ $-msg6
+
+
+SECTION .bss
+num: resb 5
+num1: resb 6
+choice: resb 2
+n: resb 1
+count: resb 2
+count1: resb 2
+count2: resb 2
+cnt: resb 2
+result: resb 5
+result1: resb 4
+res: resb 8
+
+
+SECTION .text
+global _start
+_start:
+menu:
+print msg6,len6
+accept choice,2  ;accepts choice of the user
+xor rdx,rdx
+xor rax,rax
+xor rcx,rcx
+xor rbx,rbx
+cmp byte[choice],31H
+je ch1
+cmp byte[choice],32H
+je ch2
+cmp byte[choice],33H
+je exit
+
+ch1:     ;to convert hex to BCD
+print msg1,len1
+accept num,5  ;input 4 digit hex number
+call asc_hex4
+mov byte[count],0
+mov ax,bx
+mov r14,result
+
+loop1:
+mov cx,00
+mov bx,0AH
+xor rdx,rdx
+div bx   ;divide ax by bx
+push dx  ;push remainder in stack
+mov cx,ax
+inc byte[count]
+xor rdx,rdx
+xor rax,rax
+mov ax,cx  ;move quotient in ax
+cmp ax,0
+jnz loop1
+
+hex_asc5:
+pop ax  ;pop remainder from stack
+add ax,30H  ;convert to ASCII
+mov [r14],ax  ;store converted value in result
+inc r14
+dec byte[count]
+jnz hex_asc5
+
+print msg2,len2
+print result,5  ;print the result
+print msg5,len5
+jmp menu
+
+ch2:     ;to convert bcd to hex
+print msg3,len3
+accept num1,6  ;input 5 digit BCD number
+call asc_hex5
+mov byte[count],5
+mov byte[count1],5
+mov byte[cnt],4
+
+xor rax,rax
+xor rdx,rdx
+mov eax,ebx
+xor rbx,rbx
+
+loop11:
+mov dl,al
+and dx,0Fh ;to get the lsb
+push dx  ;push lsb in stack
+shr eax,04  ;shift right to get next lsb
+dec byte[count1]
+jnz loop11
+
+xor rcx,rcx
+mov ecx,10
+loop22:
+xor rax,rax
+xor rdx,rdx
+pop ax  
+;mov ecx,10
+mov dl,[count]
+dec dl
+mov byte[cnt],dl
+
+l_mul:
+cmp byte[cnt],0
+jz l_add
+mul ecx  ;multiply eax with ecx
+dec byte[cnt]
+jmp l_mul
+
+l_add:
+add rbx,rax
+dec byte[count]
+jnz loop22
+
+hex_asc4:  ;to convert result to ASCII
+mov r9,result1
+mov byte[count2],4
+xor rdx,rdx
+l1:
+    rol bx,04H
+    mov dl,bl
+    and dl,0FH
+    cmp dl,09H
+    jbe l2
+    add dl,07H
+    l2:
+    add dl,30H
+    mov[r9],dl
+    inc r9
+    dec byte[count2]
+    jnz l1
+
+print msg4,len4
+print result1,4  ;print the result
+print msg5,len5
+jmp menu
+
+
+exit:
+mov rax,60
+mov rdi,1
+syscall
+
+asc_hex4: ;procedure to convert to 4 digit input to hex
+mov ch,04
+mov bx,00
+mov r10,0
+loop:
+    mov al,[num+r10]
+    cmp al,61H
+    jb next
+    sub al,57H
+    jmp next2
+next:
+    cmp al,41h
+    jb next1
+    sub al,37H
+    jmp next2
+next1:
+     sub al,30H
+next2:
+     shl bx,04H
+     add bx,ax
+     inc r10
+     dec ch
+     jnz loop
+ret
+
+asc_hex5:  ;procedure to convert to 5 digit input to hex
+mov ch,05
+mov ebx,00
+mov r10,0
+lp:
+    mov al,[num1+r10]
+    cmp al,61H
+    jb nxt
+    sub al,57H
+    jmp nxt2
+nxt:
+    cmp al,41h
+    jb nxt1
+    sub al,37H
+    jmp nxt2
+nxt1:
+     sub al,30H
+nxt2:
+     shl ebx,04H
+     add ebx,eax
+     inc r10
+     dec ch
+     jnz lp
+ret
+
+
